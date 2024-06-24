d0865a70994bfc9ee9d892a6e3dda3ee5ea8687e
viethungle1 <hunglv0801@gmail.com>
2024-06-24 14:35:19 +0700

    show list player

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,38 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export function Home () {
+    const [playerList, setPlayerList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/players').then((response) => {
+            setPlayerList(response.data);
+        })
+    }, []);
     return(
         <>
-            <h1>Home page</h1>
+            <h1>Danh sách cầu thủ</h1>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">STT</th>
+                    <th scope="col">Tên</th>
+                    <th scope="col">Năm sinh</th>
+                    <th scope="col">Vị trí chơi</th>
+                </tr>
+                </thead>
+                <tbody className="table-group-divider">
+                {playerList.map((item, index) => (
+                    <tr key={index}>
+                        <th scope="row">{index + 1}
+                        </th>
+                        <td>{item.name}</td>
+                        <td>{item.doB}</td>
+                        <td>{item.positions.map(position => position.name).join(', ')}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </>
     )
 }
--- src/page/Login.js
+++ src/page/Login.js
@@ -2,6 +2,10 @@ import axios from "axios";
 import {Field, Form, Formik} from "formik";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
+import {useState} from "react";
+import {Modal} from "antd";
+import {IoCloseOutline} from "react-icons/io5";
+import {Register} from "./Register";
 
 export function Login () {
     const navigate = useNavigate();
@@ -21,21 +25,55 @@ export function Login () {
         })
     }
 
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleCheck = (isCheck) => {
+        setIsModalVisible(isCheck);
+    }
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
     return (
         <>
-            <Formik initialValues={{
+            <div>
+                <Formik initialValues={{
                     email: "",
                     password: ""
-            }} enableReinitialize = {true} onSubmit={(value) => {
-                Login(value);
-            }}>
-                <Form>
-                    <h1>ĐĂNG NHẬP TÀI KHOẢN</h1>
-                    <Field name="email" type="text" placeholder="Email đăng nhập"/>
-                    <Field name="password" type="password" placeholder="Nhập mật khẩu"/>
-                    <button type="submit">Đăng nhập</button>
-                </Form>
-            </Formik>
+                }} enableReinitialize={true} onSubmit={(value) => {
+                    Login(value);
+                }}>
+                    <Form>
+                        <h1>ĐĂNG NHẬP TÀI KHOẢN</h1>
+                        <Field name="email" type="text" placeholder="Email đăng nhập"/>
+                        <Field name="password" type="password" placeholder="Nhập mật khẩu"/>
+                        <button type="submit">Đăng nhập</button>
+                    </Form>
+                </Formik>
+            </div>
+            <div>
+                <div className="h-[40px] p-2 " onClick={showModal}>
+                    <div className="flex-row">
+                        <div className="items-center ml-2 text-slate-300 text-f">
+                            Đăng ký tài khoản
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <Modal width={350} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}
+                    closeIcon={<IoCloseOutline size={24} style={{color: 'white'}}/>}>
+                    <Register handler={handleCheck}/>
+                </Modal>
+            </div>
         </>
     )
 }
